27290dfcadb8bb7f97769a7dfcdd0e7ee904617c
Invatoric io <145204041+invatoric@users.noreply.github.com>
2024-09-24 13:35:38 +0800

    Change MultiheadConv from 2D-Conv to Cat-Conv

--- modules/aux_decoder/multiHeadConv.py
+++ modules/aux_decoder/multiHeadConv.py
@@ -13,7 +13,7 @@ class MultiHeadConv(nn.Module):
 	def __init__(self,dim):
 		super().__init__()
 		self.act7 = nn.GELU()
-		self.conv2d = nn.Conv2d(in_channels=4,out_channels=1, kernel_size=7, padding=3)
+		self.conv2d = nn.Conv1d(dim * 3, dim, kernel_size=7, padding=3, groups=dim)
 		self.stacking1 = nn.ModuleList(
 			convPlugin(dim) for _ in range(3)
 		)
@@ -33,8 +33,6 @@ class MultiHeadConv(nn.Module):
 			x2 = conv(x2)
 		for conv in self.stacking3:
 			x2 = conv(x2)
-		stacks = torch.stack((x,x1,x2,x3), dim=1)  # (B, C, T) -> (B, H, C, T)
-		stacks = self.conv2d(stacks)
-		stacks.transpose(0, 1) # (B, [1], C, T) -> ([1], B, C, T)
-		selected_tensor = stacks[0] # ([1], B, C, T) -> (B, C, T)
+		selected_tensor = torch.cat((x1,x2,x3),dim=1)
+		selected_tensor = self.conv2d(selected_tensor)
 		return self.act7(selected_tensor)
